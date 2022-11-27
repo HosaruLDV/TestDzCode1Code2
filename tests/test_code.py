bf49d249c69513716e100aabd0ba936e6b808329
Kirill <kirill.potapenkov@mail.ru>
2022-11-27 19:00:29 +0300

    Napukal v code2

---
+++ tests/test_code.py
@@ -0,0 +1,8 @@
+from testdz.code import get_val
+
+
+def test_get_val():
+    assert get_val({"hello": "world"}, "hello") == "world"
+    assert get_val({"hello": "world"}, "hello", "python") == "world"
+    assert get_val({}, "hello", "python") == "python"
+
